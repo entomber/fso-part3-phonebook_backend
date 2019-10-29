e57890c85ace1feb1fd4fa8cb204948e96b5fa32
Jeffrey Yang <yang.jeffrey.y@gmail.com>
2019-10-29 11:02:51 -0700

    added frontend production build and build/deploy scripts to package.json

---
+++ build/precache-manifest.650765573362a411ad0d14dda761b481.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987a52372a322190a43811719347ed65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46b90d3ddef31cf2adb",
+    "url": "/static/js/2.d9bb48c2.chunk.js"
+  },
+  {
+    "revision": "7bcb0633ce6647881f95",
+    "url": "/static/js/main.307824f9.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
